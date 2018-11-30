592d83304275fcc118eb2195445c33414428d8fa
Justin Ramos <justin.ramos@nbcuni.com>
2018-11-30 17:17:07 -0500

    finalize knn with sqft living

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -5,13 +5,19 @@ const loadCSV = require ('./load-csv');
 let {features, labels, testFeatures, testLabels } = loadCSV('kc_house_data.csv', {
   shuffle: true,
   splitTest: 10,
-  dataColumns: ['lat', 'long', 'sqft_lot'],
+  dataColumns: ['lat', 'long', 'sqft_lot', 'sqft_living'],
   labelColumns: ['price']
 });
 
 function knn(features, labels, predictionPoint, k) {
+  const { mean, variance } = tf.moments(features, 0)
+
+  const standardizedPrediction = predictionPoint.sub(mean).div(variance.pow(0.5))
+
   return features
-	.sub(predictionPoint)
+  .sub(mean)
+  .div(variance.pow(0.5))
+	.sub(standardizedPrediction)
 	.pow(2)
 	.sum(1)
 	.pow(0.5)
