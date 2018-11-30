56736dc889b9d6dfc0892591bbc76c549662daa3
Justin Ramos <justin.ramos@nbcuni.com>
2018-11-30 18:26:33 -0500

    gradient descent works

---
+++ regressions/index.js
@@ -0,0 +1,20 @@
+require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs');
+const loadCSV = require ('./load-csv');
+const LinearRegression = require('./linear-regressions')
+
+let {features, labels, testFeatures, testLabels} = loadCSV('cars.csv', {
+  shuffle: true,
+  splitTest: 50,
+  dataColumns: ['horsepower'],
+  labelColumns: ['mpg']
+});
+
+const regression = new LinearRegression(features, labels, {
+  learningRate: 0.001,
+  iterations: 1
+});
+
+regression.train();
+
+console.log('Updated M is:', regression.m, 'Updated B is:', regression.b);
