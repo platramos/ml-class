9190518f554629c189c5197c487ee9baf00865db
Justin Ramos <justin.ramos21@gmail.com>
2018-12-04 08:04:04 -0500

    Complete vectorized gradient descent algo

--- regressions/index.js
+++ regressions/index.js
@@ -1,9 +1,14 @@
 require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
-const loadCSV = require ('./load-csv');
+const loadCSV = require('./load-csv');
 const LinearRegression = require('./linear-regressions')
 
-let {features, labels, testFeatures, testLabels} = loadCSV('cars.csv', {
+let {
+  features,
+  labels,
+  testFeatures,
+  testLabels
+} = loadCSV('cars.csv', {
   shuffle: true,
   splitTest: 50,
   dataColumns: ['horsepower'],
@@ -17,4 +22,4 @@ const regression = new LinearRegression(features, labels, {
 
 regression.train();
 
-console.log('Updated M is:', regression.m, 'Updated B is:', regression.b);
+console.log('Updated M is:', regression.weights.get(1, 0), 'Updated B is:', regression.weights.get(0, 0));
