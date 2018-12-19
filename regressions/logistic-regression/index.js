810e082405e9196530351d74cd189f601c55a013
Justin Ramos <justin.ramos@nbcuni.com>
2018-12-19 15:08:34 -0500

    implement cross entropy cost function

--- regressions/logistic-regression/index.js
+++ regressions/logistic-regression/index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require('@tensorflow/tfjs');
 const loadCSV = require('../load-csv');
 const LogisticRegression = require('./logistic-regression');
+const plot = require('node-remote-plot')
 
 const {features, labels, testFeatures, testLabels} = loadCSV('../data/cars.csv', {
   dataColumns: ['horsepower', 'displacement', 'weight'],
@@ -24,3 +25,7 @@ const regression = new LogisticRegression(features, labels, {
 
 regression.train();
 console.log(regression.test(testFeatures, testLabels));
+
+plot({
+  x: regression.costHistory.reverse()
+});
