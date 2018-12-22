6436e7a360f53760a8dd7c33cd8287cac802d029
Justin Ramos <justin.ramos@nbcuni.com>
2018-12-21 20:01:48 -0500

    optimize memory usage for Tensorflow and array memory retention

--- regressions/multi-nominal-logistic-regression/logistic-regression.js
+++ regressions/multi-nominal-logistic-regression/logistic-regression.js
@@ -25,7 +25,7 @@ class LogisticRegression {
       .matMul(differences)
       .div(features.shape[0]);
 
-    this.weights = this.weights.sub(slopes.mul(this.options.learningRate));
+    return this.weights.sub(slopes.mul(this.options.learningRate));
   }
 
   train() {
@@ -36,17 +36,19 @@ class LogisticRegression {
           const startingIndex = j * this.options.batchSize;
           const { batchSize } = this.options;
 
-          const featureSlice = this.features.slice(
-            [startingIndex, 0],
-            [batchSize, -1]
-          );
+          this.weights = tf.tidy(() => {
+            const featureSlice = this.features.slice(
+              [startingIndex, 0],
+              [batchSize, -1]
+            );
 
-          const labelSlice = this.labels.slice(
-            [startingIndex, 0],
-            [batchSize, -1]
-          );
+            const labelSlice = this.labels.slice(
+              [startingIndex, 0],
+              [batchSize, -1]
+            );
 
-          this.gradientDescent(featureSlice, labelSlice);
+            return this.gradientDescent(featureSlice, labelSlice);
+          });
       }
 
       this.recordCost();
@@ -105,24 +107,27 @@ class LogisticRegression {
   }
 
   recordCost() {
-    const guesses = this.features.matMul(this.weights).softmax();
+    const cost = tf.tidy(() => {
+      const guesses = this.features.matMul(this.weights).sigmoid();
 
-    const termOne = this.labels.transpose().matMul(guesses.log());
-    const termTwo = this.labels
-      .mul(-1)
-      .add(1)
-      .transpose()
-      .matMul(
-        guesses
-          .mul(-1)
-          .add(1)
-          .log()
-      );
+      const termOne = this.labels.transpose().matMul(guesses.log());
+      const termTwo = this.labels
+        .mul(-1)
+        .add(1)
+        .transpose()
+        .matMul(
+          guesses
+            .mul(-1)
+            .add(1)
+            .log()
+        );
 
-    const cost = termOne.add(termTwo)
-      .div(this.features.shape[0])
-      .mul(-1)
-      .get(0,0)
+      return termOne
+        .add(termTwo)
+        .div(this.features.shape[0])
+        .mul(-1)
+        .get(0,0)
+    });
 
     this.costHistory.unshift(cost);
   }
